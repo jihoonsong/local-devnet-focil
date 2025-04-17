40d46fce8de557bdd585a37ec50d36234895ebe3
Jihoon Song <jihoonsong@users.noreply.github.com>
2025-04-17 20:22:57 +0900

    Add helper function to remove extended attributes in macOS Ventura and later

--- prysm/build-focil.sh
+++ prysm/build-focil.sh
@@ -4,6 +4,19 @@ set -euo pipefail
 
 ARCH="$(uname -m)"
 
+# Function to clean extended attributes from a tarball and repack it.
+clean_and_repack_tarball() {
+  local tarball_path="$1"
+  local temp_dir=$(mktemp -d ./temp_dir)
+  local temp_tar="./temp.tar"
+  
+  # Important: if you're on macOS Ventura or later, make sure you use gnu-tar not bsdtar.
+  tar -xf "$tarball_path" -C "$temp_dir"
+  tar -cf "$temp_tar" -C "$temp_dir" .
+  mv -f "$temp_tar" "$tarball_path"
+  rm -rf "$temp_dir" "$temp_tar"
+}
+
 # Build Docker images of beacon-chain and validator.
 case "$ARCH" in
   amdx86_64)
@@ -17,6 +30,9 @@ case "$ARCH" in
     bazel build //cmd/beacon-chain:oci_image_tarball --platforms=@io_bazel_rules_go//go/toolchain:linux_arm64_cgo --config=release
     echo "Building validator for ARM architecture (including Apple M1/M2/M3)..."
     bazel build //cmd/validator:oci_image_tarball  --platforms=@io_bazel_rules_go//go/toolchain:linux_arm64_cgo --config=release
+    echo "Cleaning and repacking tarballs..."
+    clean_and_repack_tarball "bazel-bin/cmd/beacon-chain/oci_image_tarball/tarball.tar"
+    clean_and_repack_tarball "bazel-bin/cmd/validator/oci_image_tarball/tarball.tar"
     ;;
   *)
     echo "Unsupported architecture: $ARCH"
